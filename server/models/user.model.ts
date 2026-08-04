7a389c42ecaf0902933fb6590e1b648259138c99
subham chakraborty <chakrabortysubham448@gmail.com>
2026-08-04 18:40:39 +0530

    modify the schema in user model

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,17 +1,15 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
-    username: string;
+    Full_name: string;
     email: string;
     password: string;
     tokens: string[];
-    createdAt: Date;
-    updatedAt: Date;
 }
 
 const userSchema: Schema<IUser> = new Schema(
     {
-        username: { type: String, required: true, unique: true },
+        Full_name: { type: String, required: true},
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         tokens: { type: [String], default: [] },
