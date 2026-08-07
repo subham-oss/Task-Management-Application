60b64e3acd07df9a91eb5dcb24d06fb31258d1b4
subham chakraborty <chakrabortysubham448@gmail.com>
2026-08-07 14:15:13 +0530

    add api in the server.ts

--- server/server.ts
+++ server/server.ts
@@ -2,15 +2,17 @@ import express, { type Express, type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB  from './config/db.ts';
+import { userRouter } from './routes/user.route.ts';
 
 dotenv.config();
-
 
 
 const app: Express = express();
 app.use(express.json());
 const port = 3000;
 connectDB();
+app.use(cors());
+app.use('/api/user', userRouter);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
