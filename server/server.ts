bd7b704e8418ebc36a34719d9332c3aa2e55693a
subham chakraborty <chakrabortysubham448@gmail.com>
2026-08-12 12:18:16 +0530

    redefine the port

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT;
 connectDB();
 app.use(cors()); 
 app.use('/api/user', userRouter);
