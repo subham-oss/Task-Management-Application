0616baba1ea1ef71f0d7a60785bbff8971ad6ab1
subham chakraborty <chakrabortysubham448@gmail.com>
2026-08-28 20:07:32 +0530

    add auth.middleware

--- server/middleware/auth.middleware.ts
+++ server/middleware/auth.middleware.ts
@@ -1,19 +1,58 @@
-import { type Request, type Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-
-export const authMiddleware = (req: Request, res: Response, next: () => void) => {
-    try{
-        const token = req.headers.authorization?.split(" ")[1];
-        if (!token) {
-            return res.status(401).json({ message: "No token provided" });
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-        req.body = decoded;
-        next();
-    }
-    catch(err: any){
-        res.status(500).json({ message: err.message });
-    }
-    
+interface JwtPayload {
+  userId: string;
+  email: string;
 }
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: JwtPayload;
+    }
+  }
+}
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({
+        message: "Access token required",
+      });
+    }
+
+    const accessToken = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      accessToken,
+      process.env.JWT_ACCESS_SECRET as string
+    ) as JwtPayload;
+
+    req.user = decoded;
+
+    next();
+  } catch (err: any) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({
+        message: "Access token expired",
+      });
+    }
+
+    if (err.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        message: "Invalid access token",
+      });
+    }
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
