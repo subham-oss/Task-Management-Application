0dcf5276f77484fea96054a3ac5a91f7224a18cf
subham chakraborty <chakrabortysubham448@gmail.com>
2026-08-29 12:39:02 +0530

    create creatTask controller

---
+++ server/controllers/task.controller.ts
@@ -0,0 +1,41 @@
+import type { Request, Response } from "express";
+import  Task  from "../models/Task.model.ts";
+
+
+export const createTask = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const {
+      Task_Title,
+      Task_Description,
+      Initial_Phase_State,
+      Severity_Index,
+      Target_Delivery_Date,
+    } = req.body;
+
+    const task = await Task.create({
+      userId,
+      Task_Title,
+      Task_Description,
+      Initial_Phase_State,
+      Severity_Index,
+      Target_Delivery_Date,
+    });
+
+    return res.status(201).json({
+      message: "Task created successfully",
+      task,
+    });
+  } catch (err: any) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
