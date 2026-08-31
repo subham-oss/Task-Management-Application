4afe797aa6bf2d94087957b0b00410d65c72dca0
subham chakraborty <chakrabortysubham448@gmail.com>
2026-08-31 20:18:22 +0530

    add gettasks route

--- server/routes/task.route.ts
+++ server/routes/task.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createTask } from '../controllers/task.controller.ts';
+import { createTask,getTasks } from '../controllers/task.controller.ts';
 import {authMiddleware} from '../middleware/auth.middleware.ts';
 
 const Taskrouter = Router();
 
 Taskrouter.post('/createtask', authMiddleware, createTask);
+Taskrouter.get('/gettasks', authMiddleware, getTasks);
 
 export default Taskrouter;
