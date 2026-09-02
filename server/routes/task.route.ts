bb44af5e27a1eecf26a65cf34186956980727757
subham chakraborty <chakrabortysubham448@gmail.com>
2026-09-02 12:17:57 +0530

    add edit task router

--- server/routes/task.route.ts
+++ server/routes/task.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createTask,getTasks } from '../controllers/task.controller.ts';
+import { createTask,getTasks,editTask } from '../controllers/task.controller.ts';
 import {authMiddleware} from '../middleware/auth.middleware.ts';
 
 const Taskrouter = Router();
 
 Taskrouter.post('/createtask', authMiddleware, createTask);
 Taskrouter.get('/gettasks', authMiddleware, getTasks);
+Taskrouter.put('/edittask/:taskId', authMiddleware, editTask);
 
 export default Taskrouter;
