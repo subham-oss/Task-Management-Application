c4471ca75a7f20329d894cecf6095fa5cb18b583
subham chakraborty <chakrabortysubham448@gmail.com>
2026-09-05 18:23:45 +0530

    add delete router

--- server/routes/task.route.ts
+++ server/routes/task.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createTask,getTasks,editTask } from '../controllers/task.controller.ts';
+import { createTask,getTasks,editTask,deleteTask } from '../controllers/task.controller.ts';
 import {authMiddleware} from '../middleware/auth.middleware.ts';
 
 const Taskrouter = Router();
@@ -7,5 +7,5 @@ const Taskrouter = Router();
 Taskrouter.post('/createtask', authMiddleware, createTask);
 Taskrouter.get('/gettasks', authMiddleware, getTasks);
 Taskrouter.put('/edittask/:taskId', authMiddleware, editTask);
-
+Taskrouter.delete('/deletetask/:taskId', authMiddleware, deleteTask);
 export default Taskrouter;
