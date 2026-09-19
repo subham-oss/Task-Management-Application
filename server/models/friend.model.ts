d37a0a5572e420d55ee99a15be52558bf583d5a1
subham chakraborty <chakrabortysubham448@gmail.com>
2026-09-19 18:25:26 +0530

    add friend schema

---
+++ server/models/friend.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IFriend extends Document {
+    requester: mongoose.Types.ObjectId;
+    receiver: mongoose.Types.ObjectId;
+    status: 'pending' | 'accepted' | 'rejected';
+    createdAt: Date;
+}
+
+const friendSchema = new Schema(
+  {
+    requester: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Friend = mongoose.model<IFriend>("Friend", friendSchema);
+export default Friend;
