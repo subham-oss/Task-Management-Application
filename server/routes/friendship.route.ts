15d59137629361843b474d374c493cd2fd81d62f
subham chakraborty <chakrabortysubham448@gmail.com>
2026-09-21 18:27:22 +0530

    create getfriend route

--- server/routes/friendship.route.ts
+++ server/routes/friendship.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import {sendFriendRequest,acceptFriendRequest} from '../controllers/FriendRequest.controller.ts';
+import {sendFriendRequest,acceptFriendRequest,getfriend} from '../controllers/FriendRequest.controller.ts';
 import {authMiddleware} from '../middleware/auth.middleware.ts';
 
 const Friendrouter = Router();
 Friendrouter.post('/sendfriendrequest', authMiddleware, sendFriendRequest);
 Friendrouter.post('/acceptfriendrequest', authMiddleware, acceptFriendRequest);
+Friendrouter.get('/getfriend', authMiddleware, getfriend);
 export default Friendrouter;
