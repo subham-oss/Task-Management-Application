8c05ff26b837b3041cc458c43bc06cc0ed7437e1
subham chakraborty <chakrabortysubham448@gmail.com>
2026-09-22 18:33:29 +0530

    add rejectFriendRequest route

--- server/routes/friendship.route.ts
+++ server/routes/friendship.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import {sendFriendRequest,acceptFriendRequest,getfriend} from '../controllers/FriendRequest.controller.ts';
+import {sendFriendRequest,acceptFriendRequest,getfriend,rejectFriendRequest} from '../controllers/FriendRequest.controller.ts';
 import {authMiddleware} from '../middleware/auth.middleware.ts';
 
 const Friendrouter = Router();
 Friendrouter.post('/sendfriendrequest', authMiddleware, sendFriendRequest);
 Friendrouter.post('/acceptfriendrequest', authMiddleware, acceptFriendRequest);
+Friendrouter.post('/rejectfriendrequest', authMiddleware, rejectFriendRequest);
 Friendrouter.get('/getfriend', authMiddleware, getfriend);
 export default Friendrouter;
